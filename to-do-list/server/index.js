3850d6f3fb4f7b997db363b90206bb2ee946c239
Bruno Scherer <brscherer@lendingclub.com>
2023-08-09 09:38:21 -0300

    fix htmx todo app

--- to-do-list/server/index.js
+++ to-do-list/server/index.js
@@ -12,25 +12,31 @@ let todos = [
   }
 ];
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/add', (req, res) => {
-  console.log(req.body)
+  if (!req.body.todo) {
+    res.status(400).json({ success: false, message: 'Type a to-do' });
+    return;
+  }
   
-  // if (!req.body.todo) {
-  //   res.status(400).json({ success: false, message: 'Type a to-do' });
-  //   return;
-  // }
-  
-  // const todoText = req.body.todo;
-  const todo = { id: todos.length + 1, text: "hard-coded todo" };
+  const todo = { id: todos.length + 1, text: req.body.todo };
   todos.push(todo);
-  res.json({ success: true, message: 'To-do added' });
+  res.render("todo-item", { todos });
 });
 
 app.get('/todos', (req, res) => {
-  res.json(todos);
+  res.render("todo-item", { todos });
+});
+
+app.get('/clear', (req, res) => {
+  todos = []
+  res.render("todo-item", { todos });
 });
 
 app.listen(port, () => {
